dd4781fbb1064a879afafc42acd5c581c867f654
Zihan Yang <zihan_yang_1996@outlook.com>
2024-08-09 17:00:40 -0400

    Call GameHasEnded for all controller, using TAcotrRange to loop over all controllers

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -3,6 +3,8 @@
 
 #include "KillEmAllGameMode.h"
 
+#include "EngineUtils.h"
+
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
 	Super::PawnKilled(PawnKilled);
@@ -10,8 +12,24 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 	APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
 	if (PlayerController != nullptr)
 	{
-		PlayerController->GameHasEnded(nullptr, false);
+		EndGame(false);
 		DisplayLoseScreen();
 	}
+}
 
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+	for (AController* Controller : TActorRange<AController>(GetWorld()))
+	{
+		if (Controller->IsPlayerController())
+		{
+			// If the controller is a player controller, then bIsWinner should be the same as bIsPlayerWinner
+			Controller->GameHasEnded(Controller->GetPawn(), bIsPlayerWinner);
+		}
+		else
+		{
+			// If the controller is not a player controller, then bIsWinner should be the opposite of bIsPlayerWinner
+			Controller->GameHasEnded(Controller->GetPawn(), !bIsPlayerWinner);
+		}
+	}
 }
